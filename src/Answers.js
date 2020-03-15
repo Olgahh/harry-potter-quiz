cf9df7b7e78903b0806f47dd75075bbe3de0d655
Olga <hijazin.olga@gmail.com>
2020-03-14 17:47:21 -0800

    Quiz

---
+++ src/Answers.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class Answers extends Component {
+  state = {
+    choices: this.props.choices,
+    answer: ""
+  };
+
+  render() {
+    return (
+      <div className="row justify-content-center">
+        <div className="card w-75 pr-4 pl-4 mb-4 mt=3 pt=3 ">
+          <div className="card-body text-center">
+            <h3 className="card-title">{this.props.question}</h3>
+
+            <img src={this.props.image} className="card-img-top" alt="..." />
+          </div>
+          {this.state.choices.map((choice, index) => (
+            <button
+              key={index}
+              type="button"
+              className={`btn btn-${
+                this.props.showAnswer && choice === this.props.correct
+                  ? "success"
+                  : "danger"
+              } mb-3 mr-3 ml-3`}
+            >
+              {choice}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Answers;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Data from "./Data";
+import Question from "./Question";
+import Result from "./Result";
+import bg from "./images/bg.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Answers from "./Answers";
+class App extends Component {
+  state = {
+    questionsData: Data(),
+    score: 0,
+    attempts: 0
+    // answers: []
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  getQuestions = () => {
+    this.setState({
+      questionsData: Data()
+    });
+  };
+
+  countAnswers = (answer, correct) => {
+    let score = this.state.score;
+    let attempts = this.state.attempts;
+    // let answers = this.state.answers;
+    if (answer === correct) {
+      ++score;
+    }
+    // answers = answers.push(answer);
+    this.setState({
+      score: score,
+      attempts: ++attempts
+      // answers: answers
+    });
+  };
+
+  resetGame = () => {
+    this.setState({
+      score: 0,
+      attempts: 0,
+      questionsData: []
+    });
+    this.getQuestions();
+  };
+
+  render() {
+    return (
+      <div style={{ backgroundImage: `url(${bg})` }} className="text-cent">
+        <nav class="navbar navbar-light bg-light">
+          <h2>
+            <a className="nav-link" href="#">
+              Harry Potter Quiz
+            </a>
+          </h2>
+          <button
+            type="button"
+            className="btn btn-warning"
+            onClick={this.resetGame}
+          >
+            Reload
+          </button>
+        </nav>
+
+        <div className="container mt-3">
+          {this.state.questionsData.length > 0 &&
+            this.state.attempts < 5 &&
+            this.state.questionsData.map(
+              ({ question, answers, correct, questionId, image }) => (
+                <Question
+                  question={question}
+                  choices={answers}
+                  correct={correct}
+                  // showAnswer={false}
+                  key={questionId}
+                  image={image}
+                  onSelection={answer => this.countAnswers(answer, correct)}
+                />
+              )
+            )}
+          {this.state.attempts === 5 && (
+            <Result
+              score={this.state.score}
+              reset={this.resetGame}
+              // answers={this.state.answers}
+            />
+          )}
+          {this.state.attempts === 5 &&
+            this.state.questionsData.map(
+              ({ question, answers, correct, questionId, image }) => (
+                <Answers
+                  question={question}
+                  choices={answers}
+                  correct={correct}
+                  showAnswer={true}
+                  key={questionId}
+                  image={image}
+                />
+              )
+            )}
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
